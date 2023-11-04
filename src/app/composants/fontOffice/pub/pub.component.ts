3c873165194be5edc4935c41642670439d00cb93
Youssef <yousseflouhichi24@gmail.com>
2023-11-04 18:58:07 +0100

    API done

--- src/app/composants/fontOffice/pub/pub.component.ts
+++ src/app/composants/fontOffice/pub/pub.component.ts
@@ -1,3 +1,4 @@
+import { UpperCasePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,35 +6,84 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './pub.component.html',
   styleUrls: ['./pub.component.css']
 })
-export class PubComponent implements OnInit{
+export class PubComponent implements OnInit {
   ngOnInit(): void {
-  
- 
-    const url = 'https://imdb-top-100-movies.p.rapidapi.com/';
+
+
+    const url = 'https://imdb-top-100-movies1.p.rapidapi.com/';
     const options = {
       method: 'GET',
       headers: {
-        'X-RapidAPI-Key': '06e4a3233fmsh85e767b07c4ccf3p1e2206jsn844f55a4324f',
-        'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com'
+        'X-RapidAPI-Key': 'b1be7fce16msh3c6684901e055d3p12ba3ajsn44d644f006b5',
+        'X-RapidAPI-Host': 'imdb-top-100-movies1.p.rapidapi.com'
       }
     };
-    
-        
-        fetch(url, options).then(data => {
-          return data.json(); }).then((objectData)=>{console.log(objectData[0].title) ;
-          let table="";
-          objectData.map((values)=>{
-            table+=`<tr> <td>${values.title}</td>  <td>${values.rank}</td>  <td><img src="${values.image}"></img></td> </tr>`;
-          })
-          document.getElementById("tablee").innerHTML=table;
-    
-    
+
+
+
+
+    fetch(url, options)
+      .then(response => response.json())
+      .then(data => {
+        let car = "";
+        let i = 0;
+        data.slice(0, 10).map((values) => {
+          car += `<div class="carousel-item `;
+          if (i == 0) {
+            car += 'active';
+          }
+          car += `"><img src="${values.images[2][1]}" class="d-block w-25 h-25 mx-auto" >
+          <div class="carousel-caption">
+          <h2>${i + 1}.</h2>
+          <h1>${values.title}</h1>
+      </div></div>`;
+          i++;
         })
-
-       
-
-    }
+        document.getElementById("carContainerTop").innerHTML = car;
 
 
-  
+      })
+
+
+
+
+    const url1 = 'https://moviesdatabase.p.rapidapi.com/titles/x/upcoming';
+    const options1 = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'd251f4cd49msh46c6cdc57c7ecffp19f194jsn4d667816c3d8',
+        'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+      }
+    };
+
+    fetch(url1, options1)
+      .then(response => response.json())
+      .then(data => {
+
+        let car = "";
+        let i = 0;
+        data.results.map(values => {
+
+          if (values.primaryImage != null) {
+            console.log(values.primaryImage.url)
+            car += `<div class="carousel-item `;
+            if (i == 0) {
+              car += 'active';
+            }
+            car += `"><img src="${values.primaryImage.url}" class="d-block  mx-auto" style="height:500px;width:300px">
+        <div class="carousel-caption">
+        <h1>${values.originalTitleText.text}</h1>
+        </div></div>`;
+            i++;
+          }
+        })
+        document.getElementById("carContainerUp").innerHTML = car;
+      })
+
+
+
+  }
+
+
+
 }
