ddc387de0ab1d126fec18bb24f60496ee9915849
joozzz <youssefbenfarah232@gmail.com>
2023-11-04 11:49:28 +0100

    mess

--- src/app/composants/fontOffice/liste-activite/liste-activite.component.ts
+++ src/app/composants/fontOffice/liste-activite/liste-activite.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
+import { Router } from '@angular/router';
+import { Activity } from 'src/app/classes/activity';
+import { ActivitiesService } from 'src/app/services/activities.service';
 
 @Component({
   selector: 'app-liste-activite',
   templateUrl: './liste-activite.component.html',
   styleUrls: ['./liste-activite.component.css']
 })
-export class ListeActiviteComponent {
+export class ListeActiviteComponent implements OnInit{
+
+  constructor(private service: ActivitiesService, private route: Router) { }
+  activities: Activity[];
+  activitiesAfficher: Activity[];
+
+  ngOnInit(): void {
+    this.activities = this.service.getActs();
+    this.activitiesAfficher = this.activities;
+  }
+
+
+  filtre1(cat: string) {
+    if (cat != "")
+      this.activitiesAfficher = this.activities.filter(e => e.categorie == cat)
+    else
+      this.activitiesAfficher = this.activities;
+
+  }
 
 }
--- src/app/composants/fontOffice/pub/pub.component.ts
+++ src/app/composants/fontOffice/pub/pub.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pub',
   templateUrl: './pub.component.html',
   styleUrls: ['./pub.component.css']
 })
-export class PubComponent {
+export class PubComponent implements OnInit{
+  ngOnInit(): void {
+  
+ 
+    const url = 'https://imdb-top-100-movies.p.rapidapi.com/';
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '06e4a3233fmsh85e767b07c4ccf3p1e2206jsn844f55a4324f',
+        'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com'
+      }
+    };
+    
+        
+        fetch(url, options).then(data => {
+          return data.json(); }).then((objectData)=>{console.log(objectData[0].title) ;
+          let table="";
+          objectData.map((values)=>{
+            table+=`<tr> <td>${values.title}</td>  <td>${values.rank}</td>  <td><img src="${values.image}"></img></td> </tr>`;
+          })
+          document.getElementById("tablee").innerHTML=table;
+    
+    
+        })
 
+       
+
+    }
+
+
+  
 }
