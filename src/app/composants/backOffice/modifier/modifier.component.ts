713dfd67ebff1dfdd13543b7545db03fe2d37603
Youssef <yousseflouhichi24@gmail.com>
2023-11-05 23:22:16 +0100

    qhqh

--- src/app/composants/backOffice/modifier/modifier.component.ts
+++ src/app/composants/backOffice/modifier/modifier.component.ts
@@ -8,53 +8,53 @@ import { ActivitiesService } from 'src/app/services/activities.service';
   templateUrl: './modifier.component.html',
   styleUrls: ['./modifier.component.css']
 })
-export class ModifierComponent implements OnInit{
-  constructor(private service:ActivitiesService,private activatedRoute:ActivatedRoute,
-    private route:Router){}
-  lesactivities:Activity[]=[];
+export class ModifierComponent implements OnInit {
+  constructor(private service: ActivitiesService, private activatedRoute: ActivatedRoute,
+    private route: Router) { }
+  lesactivities: Activity[] = [];
 
 
-  act:Activity;
-  idInit:number;
+  act: Activity;
+  idInit: number;
   ngOnInit(): void {
-    
-    this.lesactivities=this.service.getActs();
-    this.idInit= this.activatedRoute.snapshot.params['id'];
-    this.act=this.lesactivities.find(e => e.id==this.idInit)
-    
+
+    this.lesactivities = this.service.getActs();
+    this.idInit = this.activatedRoute.snapshot.params['id'];
+    this.act = this.lesactivities.find(e => e.id == this.idInit)
+
   }
 
-  test(id:string,int:string,date:string,lieu:string){
-    if(id=="" || int=="" || date=="" || lieu==""){
-      document.getElementById("butt").setAttribute("disabled","true")
+  test(id: string, int: string, date: string, lieu: string) {
+    if (id == "" || int == "" || date == "" || lieu == "") {
+      document.getElementById("butt").setAttribute("disabled", "true")
     }
-    else{      
+    else {
       document.getElementById("butt").removeAttribute("disabled")
-  }
-  }
-    
-  
-
-  modifier(id:string,int:string,ph:string,date:string,cat:string,lieu:string,prix:string,ch:boolean){
-    
-    if(!this.lesactivities.find(e=>e.id==Number(id))){
-
-    if(ph==""){
-      ph="assets/activity.png"
     }
-    else{     
-       ph="assets\\"+ph.split("\\")[2];
+  }
+
+
+
+  modifier(id: string, int: string, ph: string, date: string, cat: string, lieu: string, prix: string, ch: boolean) {
+
+    if (this.lesactivities.find(e => e.id == Number(id))==this.act) {
+
+      if (ph == "") {
+        ph = "assets/activity.png"
+      }
+      else {
+        ph = "assets\\" + ph.split("\\")[2];
+      }
+      this.service.modifierService(this.idInit, id, int, ph, date, cat, lieu, prix, ch)
+      this.route.navigate(['/admin/acts'])
     }
-    this.service.modifierService(this.idInit,id,int,ph,date,cat,lieu,prix,ch)
-    this.route.navigate(['/admin/acts'])
-  }
-  else{
-    alert("Id existe déjà pour une autre activitée.")
-  }
+    else {
+      alert("Id existe déjà pour une autre activitée.")
+    }
 
   }
 
-  Annuler(){
+  Annuler() {
     this.route.navigate(['/admin/acts'])
   }
 }
