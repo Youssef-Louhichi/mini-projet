4a9058ebc8ce9adc57690a6050752f6c4f5ea41f
joozzz <youssefbenfarah232@gmail.com>
2023-11-05 18:43:13 +0100

    jooz

---
+++ src/app/classes/suggestion.spec.ts
@@ -0,0 +1,7 @@
+import { Suggestion } from './suggestion';
+
+describe('Suggestion', () => {
+  it('should create an instance', () => {
+    expect(new Suggestion()).toBeTruthy();
+  });
+});
---
+++ src/app/classes/suggestion.ts
@@ -0,0 +1,3 @@
+export class Suggestion {
+    constructor(public nom:string,public mail:string,public sugg:string){}
+}
